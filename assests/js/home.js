ad0cd3eadd777ae55e15cb70d81354d15641db56
David Barraza <dbcomps@mac.com>
2022-08-26 17:03:12 -0500

    get the napster api working and sample pages

--- assests/js/home.js
+++ assests/js/home.js
@@ -1,1 +1,21 @@
-alert ()
+const tracksTemplateSource = document.getElementById('tracks-template').innerHTML;
+const tracksTemplate = Handlebars.compile(tracksTemplateSource);
+
+const $tracks = $('#tracks-container');
+
+const getTopTracks = $.get('https://api.napster.com/v2.1/tracks/top?apikey=NmI1MGU5NWEtNjYwNy00ZmMyLWEzODAtYzJjMGQ1NWNmMDQ4');
+
+getTopTracks
+  .then((response) => {
+    $tracks.html(tracksTemplate(response));
+  });
+  
+// var apiUrl = "https://api.napster.com/v2.1/tracks/top?apikey=NmI1MGU5NWEtNjYwNy00ZmMyLWEzODAtYzJjMGQ1NWNmMDQ4"
+// 
+// fetch(apiUrl).then(function(response) {
+// 	if (response.ok) {
+// 		response.json().then(function(data) {
+// 			console.log(data);
+// 		})
+// 	}
+// });
