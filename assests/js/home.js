dd09450fb752552b0ff78f91a270c499558b6b63
David Barraza <dbcomps@mac.com>
2022-08-26 19:33:56 -0500

    form input field working and api fetch to napster returns data

--- assests/js/home.js
+++ assests/js/home.js
@@ -1,11 +1,33 @@
-
-  
 var apiUrl = "https://api.napster.com/v2.1/tracks/top?apikey=NmI1MGU5NWEtNjYwNy00ZmMyLWEzODAtYzJjMGQ1NWNmMDQ4"
+var userFormEl = document.querySelector("#user-form");
+var artistInputEl = document.querySelector("#artist");
 
-fetch(apiUrl).then(function(response) {
-	if (response.ok) {
-		response.json().then(function(data) {
-			console.log(data);
-		})
+var formSubmitHandler = function(event) {
+	event.preventDefault();
+	
+	// get value from input element
+	var artistname = artistInputEl.value.trim();
+	
+	if (artistname) {
+			getArtistSongs(artistname);
+		return;
 	}
-});
+};
+
+var getArtistSongs = function(artist) {
+	// format the napster api url
+	var apiUrl = "https://api.napster.com/v2.2/search?query=" + artist + "&type=artist&apikey=NmI1MGU5NWEtNjYwNy00ZmMyLWEzODAtYzJjMGQ1NWNmMDQ4";
+	
+	// make a request to the url
+	fetch(apiUrl).then(function(response) {
+		// if request is successful
+		if (response.ok) {
+			response.json().then(function(data) {
+				console.log(data);
+			});
+		}
+	});
+	
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
