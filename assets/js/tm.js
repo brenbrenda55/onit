bcc6b32e07d4df962affe76ce7a22b5cb5fd4dcf
brenbrenda55 <108436173+brenbrenda55@users.noreply.github.com>
2022-08-30 22:02:26 -0500

    final ch 2

--- assets/js/tm.js
+++ assets/js/tm.js
@@ -148,5 +148,10 @@ var displayTMresults = function (results) {
 // for loop - looping over the weather forcast
 // appendChild - add the elements in the page
 
+
+
+
+
+
 searchEl.addEventListener("click", formSubmitHandler)
 
